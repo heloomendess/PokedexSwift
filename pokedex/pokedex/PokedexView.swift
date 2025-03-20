6ce0286b9d3f0d672666a2d89b00e4eb64eaca3f
Heloisa Mendes <108590744+heloomendess@users.noreply.github.com>
2025-03-20 10:56:35 -0300

    Update PokedexView.swift

--- pokedex/pokedex/PokedexView.swift
+++ pokedex/pokedex/PokedexView.swift
@@ -1,31 +1,37 @@
 import SwiftUI
 
 struct PokedexView: View {
+    
+    var poke: Pokemon = pokemons[24]
+    @State private var searchText = ""
+
     var body: some View {
-        
         ZStack {
             Color.red
                 .edgesIgnoringSafeArea(.all)
 
-            
             VStack(spacing: 20) {
-                
                 Text("Pokedex")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .padding(.top, 20)
 
-                
+                HStack {
+                    TextField("Pesquisar Pokemon", text: $searchText)
+                        .padding(10)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+                }
+
                 HStack(spacing: 20) {
-                    // Coluna 1
                     VStack(spacing: 20) {
-                        PokemonCard(imageName: "pikachu", pokemonName: "Pikachu")
+                        PokemonCard(imageName: "pikachu", pokemonName: "\(poke.name)")
                         PokemonCard(imageName: "bulbasaur", pokemonName: "Bulbasaur")
                         PokemonCard(imageName: "charmander", pokemonName: "Charmander")
                     }
-                    
-                    // Coluna 2
+
                     VStack(spacing: 20) {
                         PokemonCard(imageName: "squirtle", pokemonName: "Squirtle")
                         PokemonCard(imageName: "eevee", pokemonName: "Eevee")
@@ -38,23 +44,22 @@ struct PokedexView: View {
     }
 }
 
-// Componente reutilizável
 struct PokemonCard: View {
     var imageName: String
     var pokemonName: String
-    
+
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 10)
                 .fill(.white)
                 .frame(width: 150, height: 150)
-            
+
             VStack(spacing: 10) {
                 Image(imageName)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100)
-                
+
                 Text(pokemonName)
                     .font(.headline)
                     .foregroundColor(.black)
@@ -62,10 +67,10 @@ struct PokemonCard: View {
             }
         }
     }
-    
-    struct PokedexView_Previews: PreviewProvider {
-        static var previews: some View {
-            PokedexView()
-        }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
     }
 }
